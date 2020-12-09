7f34d93d81b839ae59d8d5a4e9a5531547422ae9
Daniel Okavi <74859686+OkaviDaniel@users.noreply.github.com>
2020-12-09 22:26:20 +0200

    Update GameServiece.java

--- api/GameServiece.java
+++ api/GameServiece.java
@@ -6,16 +6,22 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import gameClient.CL_Agent;
-import gameClient.CL_Pokemon;
+import gameClient.Pokemon;
 
 public class GameServiece implements game_service
  {
 	private static final long serialVersionUID = 1L;
 
 	private DWGraph_DS g;
-	private ArrayList<CL_Pokemon> pokemons;
+	private ArrayList<Pokemon> pokemons;
 	private ArrayList<CL_Agent> agents;
 	
+	
+	public GameServiece()
+	{
+		pokemons = new ArrayList<Pokemon>();
+		agents   = new ArrayList<CL_Agent>();
+	}
 	
 	@Override
 	public String getGraph() 
@@ -44,41 +50,57 @@ public class GameServiece implements game_service
 	@Override
 	public boolean addAgent(int start_node) 
 	{
+		//addAgent to the Arena/Graph
 		return false;
 	}
 
 	@Override
-	public long startGame() {
+	public long startGame() 
+	{
+		//Starting the game
 		return 0;
 	}
 
 	@Override
-	public boolean isRunning() {
+	public boolean isRunning()
+	{
+		//Thread isAlive()?
 		return false;
 	}
 
 	@Override
-	public long stopGame() {
+	public long stopGame() 
+	{
+		// Stop the Thread or something
 		return 0;
 	}
 
 	@Override
-	public long chooseNextEdge(int id, int next_node) {
+	public long chooseNextEdge(int id, int next_node) 
+	{
+		//Must think of a good algorithm for moving
+		// id=agent to the next node
 		return 0;
 	}
 
 	@Override
-	public long timeToEnd() {
+	public long timeToEnd() 
+	{
+		// Show on the screen the time until the game is ending
 		return 0;
 	}
 
 	@Override
-	public String move() {
+	public String move() 
+	{
+		//Move all the agents on the graph one move 
 		return null;
 	}
 
 	@Override
-	public boolean login(long id) {
+	public boolean login(long id) 
+	{
+		//Send the id to the server.
 		return false;
 	}
 
