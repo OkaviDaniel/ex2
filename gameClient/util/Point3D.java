cb4ff3df3550304341f1a4cdd93c61108f9a2308
Daniel Okavi <74859686+OkaviDaniel@users.noreply.github.com>
2020-12-13 23:24:29 +0200

    Add files via upload

--- gameClient/util/Point3D.java
+++ gameClient/util/Point3D.java
@@ -7,26 +7,41 @@ import api.geo_location;
 
 import java.io.Serializable;
 
-public class Point3D extends Constants implements geo_location, Serializable{
-
+public class Point3D implements geo_location, Serializable{
 	private static final long serialVersionUID = 1L;
+	/**
+     * Simple set of constants - should be defined in a different class (say class Constants).*/
+    public static final double EPS1 = 0.001, EPS2 = Math.pow(EPS1,2), EPS=EPS2;
+    /**
+     * This field represents the origin point:[0,0,0]
+     */
     public static final Point3D ORIGIN = new Point3D(0,0,0);
-    private double _x,_y,_z;
+    
+    private double x,y,z;
+    
     public Point3D(double x, double y, double z) {
-        _x=x;
-        _y=y;
-        _z=z;
+        this.x=x;
+        this.y=y;
+        this.z=z;
     }
 
-    public Point3D(Point3D p) {
+    public Point3D(Point3D p)
+    {
        this(p.x(), p.y(), p.z());
     }
-    public Point3D(double x, double y) {this(x,y,0);}
-    public Point3D(String s) { try {
+    
+    public Point3D(double x, double y)
+    {
+    	this(x,y,0);
+    }
+    
+    public Point3D(String s) 
+    { try 
+    {
             String[] a = s.split(",");
-            _x = Double.parseDouble(a[0]);
-            _y = Double.parseDouble(a[1]);
-            _z = Double.parseDouble(a[2]);
+            x = Double.parseDouble(a[0]);
+            y = Double.parseDouble(a[1]);
+            z = Double.parseDouble(a[2]);
         }
         catch(IllegalArgumentException e) {
             System.err.println("ERR: got wrong format string for POint3D init, got:"+s+"  should be of format: x,y,x");
@@ -34,14 +49,18 @@ public class Point3D extends Constants implements geo_location, Serializable{
         }
     }
     @Override
-    public double x() {return _x;}
+    public double x() {return x;}
     @Override
-    public double y() {return _y;}
+    public double y() {return y;}
     @Override
-    public double z() {return _z;}
+    public double z() {return z;}
 
 
-    public String toString() { return _x+","+_y+","+_z; }
+    public String toString() 
+    { 
+    	return x+","+y+","+z; 
+    }
+    
     @Override
     public double distance(geo_location p2) {
         double dx = this.x() - p2.x();
@@ -51,19 +70,29 @@ public class Point3D extends Constants implements geo_location, Serializable{
         return Math.sqrt(t);
     }
 
-    public boolean equals(Object p) {
-        if(p==null || !(p instanceof geo_location)) {return false;}
+    public boolean equals(Object p) 
+    {
+        if(p==null || !(p instanceof geo_location)) 
+        {
+        	return false;
+        }
         Point3D p2 = (Point3D)p;
-        return ( (_x==p2._x) && (_y==p2._y) && (_z==p2._z) );
+        return ( (x==p2.x) && (y==p2.y) && (z==p2.z) );
     }
-    public boolean close2equals(geo_location p2) {
-        return ( this.distance(p2) < EPS ); }
+    
+    public boolean close2equals(geo_location p2) 
+    {
+        return ( this.distance(p2) < EPS ); 
+       }
+    
     public boolean equalsXY (Point3D p)
-    {return p._x == _x && p._y == _y;}
+    {
+    	return p.x == x && p.y == y;
+    }
 
      public String toString(boolean all) {
-        if(all) return "[" + _x + "," +_y+","+_z+"]";
-        else return "[" + (int)_x + "," + (int)_y+","+(int)_z+"]";
+        if(all) return "[" + x + "," +y+","+z+"]";
+        else return "[" + (int)x + "," + (int)y+","+(int)z+"]";
     }
 }
 
--- gameClient/util/Range.java
+++ gameClient/util/Range.java
@@ -4,51 +4,77 @@ package gameClient.util;
  * @author boaz_benmoshe
  *
  */
-public class Range {
+public class Range 
+{
 	private double _min, _max;
-	public Range(double min, double max) {
+	
+	public Range(double min, double max)
+	{
 		set_min(min);
 		set_max(max);
 	}
-	public Range(Range x) {
+	
+	public Range(Range x) 
+	{
 		this(x._min, x._max);
 	}
 	
-	public boolean isIn(double d) {
+	public boolean isIn(double d)
+	{
 		boolean inSide = false;
-		if(d>=this.get_min() && d<=this.get_max()) {inSide=true;}
+		if(d>=this.get_min() && d<=this.get_max()) 
+		{
+			inSide=true;
+		}
 		return inSide;
 	}
-	public String toString() {
+	
+	public String toString() 
+	{
 		String ans = "["+this.get_min()+","+this.get_max()+"]";
 		if(this.isEmpty()) {ans = "Empty Range";}
 		return ans;
 	}
-	public boolean isEmpty() {
+	
+	public boolean isEmpty() 
+	{
 		return this.get_min()>this.get_max();
 	}
-	public double get_max() {
+	
+	public double get_max() 
+	{
 		return _max;
 	}
-	public double get_length() {
+	
+	public double get_length()
+	{
 		return _max-_min;
 	}
 	
-	private void set_max(double _max) {
+	private void set_max(double _max) 
+	{
 		this._max = _max;
 	}
-	public double get_min() {
+	
+	public double get_min()
+	{
 		return _min;
 	}
-	private void set_min(double _min) {
+	
+	private void set_min(double _min) 
+	{
 		this._min = _min;
 	}
-	public double getPortion(double d) {
+	
+	public double getPortion(double d) 
+	{
 		double d1 = d-_min;
 		double ans = d1/get_length();
 		return ans;
 	}
-	public double fromPortion(double p) {
+	
+	public double fromPortion(double p) 
+	{
 		return _min+p* get_length();
 	}
 }
